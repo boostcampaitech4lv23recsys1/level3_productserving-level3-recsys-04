af181117e1e13dde6f09a35135afbd63e5a6e350
KSY <zan05134@naver.com>
2023-01-09 05:59:20 +0000

    crawling path 추가해주기.

--- crawings/crawling_user.py
+++ crawings/crawling_user.py
@@ -20,12 +20,16 @@ chrome_options.add_argument('window-size= 1920,1080')
 chrome_options.add_argument('--kiosk')
 # executable_path : /opt/ml/crawling/chromedriver
 driver = webdriver.Chrome(executable_path='/opt/ml/input/project/crawings/chromedriver', chrome_options=chrome_options)
-data = pd.read_csv('./save_csv/river_behind_concat.csv')
+# ./area_csv/Gangnam/river_behind_concat.csv
+
+path = 'Gangnam' # 얘를 바꿔가자.
+
+data = pd.read_csv(f'./area_csv/{path}/river_behind_concat.csv')
 url_list = list(data['url'].values)
 userlink = pd.DataFrame()
 #url_list = ["36528615", "31317229", "1269259731", "1779562085", "33040151", "1675801137", "1384361907", "1372982703", "1681990192", "1971062401"]
-current_status = 2000
-for _url in tqdm(url_list[2000:2256]):
+current_status = 0
+for _url in tqdm(url_list[0:50]):
     try:
         driver = webdriver.Chrome(executable_path='/opt/ml/input/project/crawings/chromedriver', chrome_options=chrome_options)
         action = ActionChains(driver)
@@ -59,7 +63,7 @@ for _url in tqdm(url_list[2000:2256]):
             if count >= 60: flag = True; break
         print("click 2/2 complete")
         if flag:
-            with open("./user_csv/notsaved.txt", "a") as file:
+            with open(f"./user_csv/{path}/notsaved.txt", "a") as file:
                 file.write(f"{str(current_status)}\n")
                 file.close()
             current_status += 1
@@ -74,12 +78,12 @@ for _url in tqdm(url_list[2000:2256]):
         userlink2 = pd.DataFrame({'link' : link_list, 'user' : user_list}, dtype = str)
         userlink2['rest'] = _url
         userlink = pd.concat([userlink, userlink2], axis = 0, sort=False)
-        userlink.to_csv(f'./user_csv/river_behind{current_status}.csv', index=False)#river_behind500
-        with open("./user_csv/log.txt", "w") as file:
+        userlink.to_csv(f'./user_csv/{path}/river_behind{current_status}.csv', index=False)#river_behind500
+        with open(f"./user_csv/{path}/log.txt", "w") as file:
             file.writelines(str(current_status))
         current_status += 1
     except:
-        with open("./user_csv/notsaved.txt", "a") as file:
+        with open(f"./user_csv/{path}/notsaved.txt", "a") as file:
             file.write(f"{str(current_status)}\n")
             file.close()
         current_status += 1
