7886bdd095e1eb4d547c28f8826dc06a68e9e7a4
Seunghoon-Schini-Yang <hyuio0228@gmail.com>
2023-01-17 05:47:22 +0000

    UPDATE backend/main.py before location api

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from typing import List, Optional
+from starlette.responses import JSONResponse
 
 from model import trash_model
 
@@ -29,19 +30,19 @@ app.add_middleware(
 )
 
 
-################ mysql 설정
+################ mysql database 설정
 config = {
     'user': 'root',
     'password': 'wogud1028',
     'host': '34.64.202.234',
     'client_flags': [ClientFlag.SSL],
-    # 아래 인증키 경로들은 각자 환경에 맞게 수정
+    # 아래 인증키 경로들은 각자 환경에 맞게 수정 (언제 한번 통일 ㄱㄱ)
     'ssl_ca': '/opt/ml/input/project/db/ssl/server-ca.pem',
     'ssl_cert': '/opt/ml/input/project/db/ssl/client-cert.pem',
     'ssl_key': '/opt/ml/input/project/db/ssl/client-key.pem'
 }
 
-config['database'] = 'rest'  # add new database to config dict
+config['database'] = 'rest'  # add "rest" database to config dict
 cnxn = mysql.connector.connect(**config)
 cursor = cnxn.cursor()
 ################
@@ -84,40 +85,52 @@ class Restaurant(BaseModel):
 
 
 class User(BaseModel):
-    rest_name: str
-    rest_tag: str
-    rest_id: str
-    review: str
-    rating: str
-    count: str
     user_id: str
-
-    
-    restaurants : List[Restaurant] = Field(default_factory=list)
-
-
-    def add_restaurant(self, restaurant: Restaurant):
-        if restaurant.id in [existing_id for existing_id in self.restaurants]:
-            return self
-
-        self.restaurants.append(restaurant)
-        return self
+    location: str
+    # restaurants : List[Restaurant] = Field(default_factory=list)
 
 
+    # def add_restaurant(self, restaurant: Restaurant):
+    #     if restaurant.id in [existing_id for existing_id in self.restaurants]:
+    #         return self
 
-@app.post('/signin/{user_id}')
-def user_signin(user_id: str):
-    if user_id in users:
-        return {"message": "success"}
-    else:
-        return {"message": "not registerd"}
-
-
-# @app.get('/user/{user_id}')
-# def get_user_record(user_id: str):
-#     return [datum[1] for datum in data if datum[0] == user_id]
+    #     self.restaurants.append(restaurant)
+    #     return self
 
 
+users = ['5c667add298eafd0547442d8', '5c3737d3d764236c17947538']
+
+
+@app.post('/signin')
+def signin(user: User):
+    if user.user_id in users:
+        '''
+        user.location으로 쿼리 날려서 좌표 가져오는 코드
+        '''
+        _x = _y = 0
+
+
+        '''
+        
+        '''
+
+        return JSONResponse({"message": "success"})
+    return JSONResponse({"message": "cold-start"})
+
+
+
+def ml_model(user_id):
+    """_summary_
+
+    Args:
+        user_id (_type_): 
+    """
+    return
+
+
+
+# 모든 식당 정보 가져오는 API
+'''
 @app.get('/restaurants/', description="모든 식당 리스트를 가져옵니다")
 async def get_restaurants() -> List:
     select_sql = "select * from rest"# where rating = 4.42"
@@ -126,33 +139,29 @@ async def get_restaurants() -> List:
     # _id, _x, _y,_cat,_name,_imgurl = zip(*result)
     restaurants = result
     return restaurants
+'''
 
 
+# 특정 식당 정보 가져오는 API
 @app.get('/restaurant/{rest_id}/')
 def get_restaurant(rest_id: str):
     select_sql = f"select * from rest where id = {rest_id}"
     cursor.execute(select_sql)
     result = cursor.fetchall()
-    # _id, _x, _y,_tag,_name,_imgurl = zip(*result)
     _id, _x, _y,_tag,_name,_imgurl = result[0]
-    rest_info = Restaurant(id=_id, x=_x, y=_y, tag=_tag, name=_name, img_url=_imgurl)
-    return {"message": rest_info}
+    rest_info = dict(Restaurant(id=_id, x=_x, y=_y, tag=_tag, name=_name, img_url=_imgurl))
+    rest_info['success'] = True
+    return JSONResponse(rest_info)
 
 
 '''
 def get_restaurant_image_by_id(rest_id: str) -> Optional[Restaurant]:
     rest = Restaurant(id=rest_id)
     rest.show_image()
-'''
 
 
-'''
 def get_restaurant_by_id(user_id: str) -> Optional[User]:
     return next((restaurant for restaurant in restaurants if restaurant.id == user_id), None)
-'''
-
-
-
 
 
 class RestaurantUpdate(BaseModel):
@@ -170,8 +179,10 @@ def update_restaurant_by_id(user_id: str, restaurant_update: RestaurantUpdate):
             updated_restaurant.add_restaurant(next_restaurant)
 
         return updated_restaurant
+'''
 
 
+'''
 @app.patch('/user/{user_id}', description="새로 리뷰를 등록한 식당을 업데이트합니다")
 async def update_restaurant(user_id: str, restaurant_update: RestaurantUpdate):
     updated_restaurant = update_restaurant_by_id(user_id=user_id, restaurant_update=restaurant_update)
@@ -185,13 +196,11 @@ async def update_restaurant(user_id: str, restaurant_update: RestaurantUpdate):
 class Prediction(User):
     name: str = 'predict_result'
 
-'''
 @app.get('/predict/{user_id}/{rest_id}', description="해당 유저의 정보를 모델에게 전달하고 예측 결과를 가져옵니다")
 async def make_prediction(user_id: str, rest_id: str, model=trash_model):
     predict_result = model()
     prediction = Prediction(result=predict_result)
     return prediction
-'''
 
 
 
@@ -200,4 +209,4 @@ class Item(BaseModel):
 
 
 users = ['5c667add298eafd0547442d8', '5c3737d3d764236c17947538']
-
+'''
