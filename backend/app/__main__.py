e2548986b8a5031ba3cbd114b2f5542b018d7850
Seunghoon-Schini-Yang <hyuio0228@gmail.com>
2023-01-25 06:16:46 +0000

    merge with backend 2

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,8 +1,4 @@
 if __name__ == "__main__":
     import uvicorn
 
-<<<<<<< HEAD
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-=======
     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
->>>>>>> 5a4564129709072ac95ebb6fc25389be9f48132d
